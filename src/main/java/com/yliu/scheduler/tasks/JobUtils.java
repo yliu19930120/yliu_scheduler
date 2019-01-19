b4c6a46e81f243bede037259a5bf375181ba963b
LiuYonghua <yliu19930120@163.com>
2019-01-19 11:05:31 +0800

    修复打印日志bug

--- src/main/java/com/yliu/scheduler/tasks/JobUtils.java
+++ src/main/java/com/yliu/scheduler/tasks/JobUtils.java
@@ -1,11 +1,11 @@
 package com.yliu.scheduler.tasks;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSON;
@@ -18,18 +18,19 @@ public class JobUtils {
 	public static final Map<String,Job> KEY_JOB_MAP = new HashMap<>();
 
 	static{
-		readJson();
+		new JobUtils().readJson();
 	}
 	
-	private static void readJson(){
+	private  void readJson(){
 		String json = null;
 		
 		try {
-			 json = FileUtils.readFileToString(new File("jobs.json"));
+			 InputStream is = this.getClass().getResourceAsStream("/jobs.json");
+			 json = IOUtils.toString(is);
 			 JSONObject jsonObj = JSON.parseObject(json);
 			 List<Job> jobs = jsonObj.getJSONArray("jobs").toJavaList(Job.class);
 			 log.info("读取job数量={}",jobs.size());
-			 jobs.forEach(job->KEY_JOB_MAP.put(job.getJobId(), job));
+			 jobs.forEach(job->JobUtils.KEY_JOB_MAP.put(job.getJobId(), job));
 		} catch (IOException e) {
 			log.error("配置文件读取错误",e);
 		}
